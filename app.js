c151c8c22a0ccb7e1466b562611494ed21eb40b4
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-06-23 17:59:49 -0500

    Adding a route for root

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const http = require('http')
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers)
+  const { url } = req
   res.setHeader('Content-Type', 'text/html')
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
   res.write('<html>')
   res.write('<head><title>My First Page</title></head>')
   res.write('<body><h1>Hello from my Node.js Server</h1></body>')
