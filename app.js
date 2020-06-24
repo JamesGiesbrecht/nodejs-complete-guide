59d773805cd2cf84f796d5f65eea3f39c36df9c3
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-06-23 19:09:13 -0500

    Moving the routes to a separate file (module) and importing them

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 const http = require('http')
-const { promises: fs } = require('fs')
+
+const routes = require('./routes')
 
 const PORT = 3000
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req
-  res.setHeader('Content-Type', 'text/html')
-  if (url === '/') {
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>')
-    res.write('</html>')
-    return res.end()
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk)
-    })
-    return req.on('end', async () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      await fs.writeFile('message.txt', message)
-      res.statusCode = 302
-      res.setHeader('Location', '/')
-      return res.end()
-    })
-  }
-  res.write('<html>')
-  res.write('<head><title>My First Page</title></head>')
-  res.write('<body><h1>Hello from my Node.js Server</h1></body>')
-  res.write('</html>')
-  return res.end()
-})
+const server = http.createServer(routes)
 
 server.listen(PORT)
+// eslint-disable-next-line no-console
+console.log(`Server is live on port ${PORT}`)
