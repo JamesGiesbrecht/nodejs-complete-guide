76a898f48f490c20dafc670617da6a78e9d66adf
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 17:07:56 -0500

    Installing express

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
-const routes = require('./routes')
+const express = require('express')
 
+const app = express()
 const PORT = 3000
 
-const server = http.createServer(routes)
+const server = http.createServer(app)
 
 server.listen(PORT)
 // eslint-disable-next-line no-console
