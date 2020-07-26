b898fb05aee8cbceb4fd6d97032c7fb77bbd1e99
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 17:13:41 -0500

    Adding middleware

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+//  Add new middleware function
+//  Function will be executed on every incoming request
+app.use((req, res, next) => {
+  console.log("I'm in the middleware!")
+  //  Calling next allows it to travel to the next middleware
+  //  If its the last stop, send a response instead
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log("I'm in another middleware!")
+})
+
 const server = http.createServer(app)
 
 server.listen(PORT)
