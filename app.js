b77349c2bf2941e661f8ccfe4c4785510d2c3a93
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 20:23:19 -0500

    Filtering middleware to only run on POST requests

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/add-product', (req, res, next) => {
   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>')
 })
 
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
   console.log(req.body)
   res.redirect('/')
 })
