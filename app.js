6885e92c47bb4024b4f6c60176c4236f4ac69f65
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 20:38:38 -0500

    Adding route filtering for an entire routes file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //  Importing routes to app.js, the order still matters
-app.use(adminRoutes)
+app.use('/admin', adminRoutes) // Filtering admin routes with a /admin in the url
 app.use(shopRoutes)
 
 app.use((req, res) => {
