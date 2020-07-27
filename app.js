2c2e89dedc0ec95597e458e7def228f5fa8ee786
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 20:12:07 -0500

    Adding new route

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-//  Add new middleware function
-//  Function will be executed on every incoming request
 app.use((req, res, next) => {
-  console.log("I'm in the middleware!")
-  //  Calling next allows it to travel to the next middleware
-  //  If its the last stop, send a response instead
+  console.log('This always runs')
   next()
 })
 
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Product Added</h1>')
+})
+
 app.use((req, res, next) => {
-  console.log("I'm in another middleware!")
   res.send('<h1>Hello from Express</h1>')
 })
 
