0ccf3f0c1f9764e1ed12bd03ecef6afc451969d1
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-07-26 20:53:42 -0500

    Serving html pages on routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
 router.get('/product', (req, res) => {
-  res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'product.html'))
 })
 
 router.post('/product', (req, res) => {
