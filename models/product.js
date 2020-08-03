6a19d90e25b13660c1241484588403291d3968a7
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-03 16:42:04 -0500

    Writing/reading data from the filesystem

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = require('../util/path')
+
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +10,34 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    const p = path.join(
+      rootDir,
+      'data',
+      'products.json',
+    )
+
+    fs.promises.readFile(p, (err, fileContent) => {
+      let products = []
+      if (!err) {
+        products = JSON.parse(fileContent)
+      }
+      products.push(this)
+      fs.promises.writeFile(p, JSON.stringify(products))
+    })
   }
 
   static fetchAll() {
-    return products
+    const p = path.join(
+      rootDir,
+      'data',
+      'products.json',
+    )
+
+    fs.promises.readFile(p, (err, fileContent) => {
+      if (err) {
+        return []
+      }
+      return JSON.parse(fileContent)
+    })
   }
 }
