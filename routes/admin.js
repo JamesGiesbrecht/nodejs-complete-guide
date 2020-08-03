343ce52bdc8446a18281c8c7a78362c07aac43e3
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-03 17:42:15 -0500

    Refactoring model

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
 
-const router = express.Router()
 const productsController = require('../controllers/products')
+
+const router = express.Router()
 
 router.get('/product', productsController.getAddProduct)
 
