ab164590cd1cd8b845ca0f83a09ff802abb053d3
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-12-28 15:06:42 -0600

    Creating a default user and adding them to every request

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Middleware to add the user to every request
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((error) => console.log(error))
+})
+
 //  Importing routes to app.js, the order still matters
 app.use('/admin', adminRoutes) // Filtering admin routes with a /admin in the url
 app.use(shopRoutes)
@@ -30,10 +40,14 @@ User.hasMany(Product) // Optional
 sequelize
   // .sync({ force: true }) // Might not want to use this in prod
   .sync()
-  .then((result) => {
-    // console.log(result)
-    app.listen(PORT)
+  .then((result) => User.findByPk(1))
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'James', email: 'test@test.com' })
+    }
+    return Promise.resolve(user)
   })
+  .then((user) => app.listen(PORT))
   .catch((error) => console.log(error))
 
 // eslint-disable-next-line no-console
