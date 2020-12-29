f88a6c43a54c815922a462d06b0a7c231d132d0f
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-12-28 20:59:19 -0600

    Commenting out broken routes that were using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,64 +23,64 @@ exports.postAddProduct = (req, res) => {
     .finally(() => res.redirect('/admin/products'))
 }
 
-exports.getEditProduct = (req, res) => {
-  const editMode = req.query.edit
-  if (!editMode) return res.redirect('/')
+// exports.getEditProduct = (req, res) => {
+//   const editMode = req.query.edit
+//   if (!editMode) return res.redirect('/')
 
-  const { productId } = req.params
+//   const { productId } = req.params
 
-  req.user
-    .getProducts({ where: { id: productId } })
-  // Product.findByPk(productId)
-    .then(([product]) => {
-      if (!product) return res.redirect('/')
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product,
-      })
-    })
-    .catch((error) => console.log(error))
-}
+//   req.user
+//     .getProducts({ where: { id: productId } })
+//   // Product.findByPk(productId)
+//     .then(([product]) => {
+//       if (!product) return res.redirect('/')
+//       res.render('admin/edit-product', {
+//         pageTitle: 'Edit Product',
+//         path: '/admin/edit-product',
+//         editing: editMode,
+//         product,
+//       })
+//     })
+//     .catch((error) => console.log(error))
+// }
 
-exports.postEditProduct = (req, res) => {
-  const { productId, title, imageUrl, price, description } = req.body
-  Product.findByPk(productId)
-    .then((product) => {
-      product.title = title
-      product.imageUrl = imageUrl
-      product.price = price
-      product.description = description
-      return product.save()
-    })
-    .then((result) => {
-      // product.save then
-      console.log(`Updated Product: ${title}`)
-    })
-    .catch((error) => console.log(error))
-    .finally(() => res.redirect('/admin/products'))
-}
+// exports.postEditProduct = (req, res) => {
+//   const { productId, title, imageUrl, price, description } = req.body
+//   Product.findByPk(productId)
+//     .then((product) => {
+//       product.title = title
+//       product.imageUrl = imageUrl
+//       product.price = price
+//       product.description = description
+//       return product.save()
+//     })
+//     .then((result) => {
+//       // product.save then
+//       console.log(`Updated Product: ${title}`)
+//     })
+//     .catch((error) => console.log(error))
+//     .finally(() => res.redirect('/admin/products'))
+// }
 
-exports.postDeleteProduct = (req, res) => {
-  const { productId } = req.body
-  Product.findByPk(productId)
-    .then((product) => product.destroy())
-    .then((result) => console.log(`Destroyed product: ${productId}`))
-    .catch((error) => console.log(error))
-    .finally(() => res.redirect('/admin/products'))
-}
+// exports.postDeleteProduct = (req, res) => {
+//   const { productId } = req.body
+//   Product.findByPk(productId)
+//     .then((product) => product.destroy())
+//     .then((result) => console.log(`Destroyed product: ${productId}`))
+//     .catch((error) => console.log(error))
+//     .finally(() => res.redirect('/admin/products'))
+// }
 
-exports.getProducts = (req, res) => {
-  req.user
-    .getProducts()
-  // Product.findAll()
-    .then((products) => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-      })
-    })
-    .catch((error) => console.log(error))
-}
+// exports.getProducts = (req, res) => {
+//   req.user
+//     .getProducts()
+//   // Product.findAll()
+//     .then((products) => {
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/products',
+//       })
+//     })
+//     .catch((error) => console.log(error))
+// }
